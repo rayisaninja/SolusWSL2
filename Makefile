sileshn/ManjaroWSL2

Rename zip file

Makefile
@@ -1,4 +1,4 @@
-OUT_ZIP=Manjaro.zip
+OUT_ZIP=ManjaroWSL.zip
 LNCR_EXE=Manjaro.exe
 
 DLR=curl

rayisaninja/SolusWSL2

Makefile
@@ -1,4 +1,4 @@
-OUT_ZIP=Solus.zip
+OUT_ZIP=SolusWSL.zip
 LNCR_EXE=Solus.exe
 
 DLR=curl